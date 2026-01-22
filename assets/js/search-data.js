9c3e18cf75ce77714cc312df25bf8fc740fbf5d0
lhaegel <85337922+lhaegel@users.noreply.github.com>
2026-01-22 10:22:15 +0000

    Deploying to gh-pages from @ ricochet-experiment/ricochet-experiment.github.io@de1463a2efb8c1055a0b5412d8de2cc854cda016 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,7 +9,14 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-publications",
+  },{id: "nav-experiment",
+          title: "experiment",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/experiment/";
+          },
+        },{id: "nav-publications",
           title: "publications",
           description: "Articles from the Ricochet collaboration.",
           section: "Navigation",
