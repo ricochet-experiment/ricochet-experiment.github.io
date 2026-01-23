2eb51e9423bc53fcc804adbdf3bf144b8d6c3cc3
lhaegel <85337922+lhaegel@users.noreply.github.com>
2026-01-23 20:45:45 +0000

    Deploying to gh-pages from @ ricochet-experiment/ricochet-experiment.github.io@ecbd06ed73c5c623bb1bd6ab6234bfbe2eaedb18 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -417,6 +417,11 @@ ninja.data = [{
           description: "Photos of the Ricochet detectors.",
           section: "Projects",handler: () => {
               window.location.href = "/projects/2_project/";
+            },},{id: "projects-diagrams",
+          title: 'diagrams',
+          description: "Schematics of the Ricochet setup.",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/3_project/";
             },},{
         id: 'social-email',
         title: 'email',
